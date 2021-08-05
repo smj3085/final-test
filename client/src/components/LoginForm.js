8f2c5fcf24b917cf221ac6b6b3c0c3ca931d270d
Stephanie Lee <stephaniemjlee@gmail.com>
2021-08-05 21:14:18 +1000

    updated loginform

--- client/src/components/LoginForm.js
+++ client/src/components/LoginForm.js
@@ -1,57 +1,67 @@
+// see SignupForm.js for comments
 import React, { useState } from 'react';
-import Auth from '../utils/auth';
-import { Button, Form, Grid, Header, Segment } from 'semantic-ui-react';
+// import { Form, Button, Alert } from 'react-bootstrap';
+import { Button, Form, Grid, Header, Segment, Message } from 'semantic-ui-react';
 import {FaPlaneDeparture } from 'react-icons/fa';
-import { LOGIN_USER } from '../utils/mutations';
-import { useMutation } from '@apollo/react-hooks';
-import { Alert } from 'react-bootstrap';
+import { loginUser } from '../utils/API';
+import Auth from '../utils/auth';
 
 const LoginForm = () => {
-    const [login, {error} ] = useMutation(LOGIN_USER);
-    const [userFormData, setUserFormData] = useState({ email: '', password: '' });
-    const [validated] = useState(false);
-    const [showAlert, setShowAlert] = useState(false);
+  const [userFormData, setUserFormData] = useState({ email: '', password: '' });
+  const [validated] = useState(false);
+  const [showAlert, setShowAlert] = useState(false);
 
-    const handleInputChange = (event) => {
-        const { name, value } = event.target;
-        setUserFormData({
-            ...userFormData,
-            [name]: value
-        });
-    };
+  const handleInputChange = (event) => {
+    const { name, value } = event.target;
+    setUserFormData({ ...userFormData, [name]: value });
+  };
 
-    const handleFormSubmit = async event => {
-        event.preventDefault();
-        try {
-            const { data } = await login({
-                variables: {...userFormData}
-            });
-            Auth.login(data.login.token);
-        } catch (error) {
-            console.error(error);
-            setShowAlert(true);
-        }
+  const handleFormSubmit = async (event) => {
+    event.preventDefault();
 
-        setUserFormData({
-            username: '',
-            email: '',
-            password: '',
-        });
-    };
+    // check if form has everything (as per react-bootstrap docs)
+    const form = event.currentTarget;
+    if (form.checkValidity() === false) {
+      event.preventDefault();
+      event.stopPropagation();
+    }
 
-    return (
-        <>
-        <Grid textAlign='center' style={{ height: '80vh' }} verticalAlign='middle'>
+    try {
+      const response = await loginUser(userFormData);
+
+      if (!response.ok) {
+        throw new Error('something went wrong!');
+      }
+
+      const { token, user } = await response.json();
+      console.log(user);
+      Auth.login(token);
+    } catch (err) {
+      console.error(err);
+      setShowAlert(true);
+    }
+
+    setUserFormData({
+      username: '',
+      email: '',
+      password: '',
+    });
+  };
+
+  return (
+    <>
+    <Grid textAlign='center' style={{ height: '80vh' }} verticalAlign='middle'>
         <Grid.Column style={{ maxWidth: 450 }}>
           <Header as='h2' color='black' textAlign='center'>
             <FaPlaneDeparture /> Log-in to your account
           </Header>
-          <Form size='large' noValidate validated={validated} onSubmit={handleFormSubmit}>
-            <Alert dismissible onClose={() => setShowAlert(false)} show={showAlert} variant='danger'>
-              Please check your email and password!
-            </Alert>
-            <Segment stacked>
-              <Form.Input 
+
+        <Form size='large' noValidate validated={validated} onSubmit={handleFormSubmit}>
+        <Message negative dismissible onClose={() => setShowAlert(false)} show={showAlert} variant='danger'>
+          <Message.Header>Something went wrong with your login credentials!</Message.Header>
+        </Message>
+        <Segment stacked>
+          <Form.Input 
                 htmlFor='email'
                 fluid icon='user' 
                 iconPosition='left' 
@@ -60,8 +70,8 @@ const LoginForm = () => {
                 onChange={handleInputChange}
                 value={userFormData.email}
                 required
-                />
-              <Form.Input
+          />
+          <Form.Input
                 htmlFor='password'
                 fluid icon='lock'
                 iconPosition='left'
@@ -71,19 +81,17 @@ const LoginForm = () => {
                 onChange={handleInputChange}
                 value={userFormData.password}
                 required
-              />
-
-              <Button color='yellow' fluid size='large' type='submit'
+          />
+        <Button color='yellow' fluid size='large' type='submit'
               disabled={!(userFormData.email && userFormData.password)}>
                 Login
               </Button>
-              {error && <div>Login failed</div>}
-            </Segment>
+              </Segment>
           </Form>
         </Grid.Column>
       </Grid>
-  </>
-    );
+    </>
+  );
 };
 
 export default LoginForm;

289eafa4f7f6623cab06375a96adfb7781eab591
Stephanie Lee <stephaniemjlee@gmail.com>
2021-08-05 20:45:55 +1000

    added footer

---
+++ client/src/components/Footer.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import { useLocation, useHistory } from 'react-router-dom';
+import { FaPlane } from 'react-icons/fa';
+
+const Footer = () => {
+  const location = useLocation();
+  const history = useHistory();
+
+  return (
+    <footer className="footer font-header" sticky="bottom">
+      <div>
+        {location.pathname !== '/' && (
+          <button
+            className="btn btn-dark mb-3"
+            onClick={() => history.goBack()}
+          >
+            &larr; Go Back
+          </button>
+        )}
+        <h6>
+          Developed by Travel with me <FaPlane />
+        </h6>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;

951973b76aff33c4639037e13aa1946a0ebf68b4
Stephanie Lee <stephaniemjlee@gmail.com>
2021-08-05 23:21:02 +1000

    fixed  expenses history table

--- client/src/pages/Expenses.js
+++ client/src/pages/Expenses.js
@@ -6,19 +6,28 @@ import { TransactionList } from '../components/Expenses/TransactionList';
 import { AddTransaction } from '../components/Expenses/AddTransaction';
 
 import { GlobalProvider } from '../components/Expenses/context/GlobalState';
+import { Divider, Grid, Segment } from 'semantic-ui-react'
+import {FaMoneyBill} from 'react-icons/fa';
 
 
 function Expenses() {
   return (
     <GlobalProvider>
-    
-      <div className="expenses-container">
+      <Segment className="expenses-container" style={{padding:'50px'}}>
         <Header />
-        <Balance />
-        <IncomeExpenses />
-        <TransactionList />
-        <AddTransaction />
-      </div>
+        <Grid columns={2} relaxed='very' stackable>
+            <Grid.Column>
+                <Balance />
+                <IncomeExpenses />
+            </Grid.Column>
+            <Grid.Column verticalAlign='middle'>
+                <TransactionList />
+                <AddTransaction />
+            </Grid.Column>
+        </Grid>
+        <Divider vertical><FaMoneyBill large />
+        </Divider>
+    </Segment>
     </GlobalProvider>
   );
 }

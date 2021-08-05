38e20ecda34f31623c96589f7ac85919dbcb8cec
Stephanie Lee <stephaniemjlee@gmail.com>
2021-08-05 21:52:49 +1000

    added expense header and balance

---
+++ client/src/components/Expenses/Header.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import {Grid} from 'semantic-ui-react';
+
+export const Header = () => {
+  return (
+    <Grid className='expense'>
+      <h2>Expense Tracker</h2>
+    </Grid>
+  )
+}

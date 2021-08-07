72a938c33e3c27a618f86db295911a4434c7600a
Stephanie Lee <stephaniemjlee@gmail.com>
2021-08-07 21:49:21 +1000

    jwt auth

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,13 @@
 import React from 'react';
-import { ApolloProvider } from '@apollo/react-hooks';
-import ApolloClient, { InMemoryCache } from 'apollo-boost';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  createHttpLink,
+} from '@apollo/client';
+import { setContext } from '@apollo/client/link/context';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
 import SearchBooks from './pages/SearchBooks';
@@ -13,19 +19,30 @@ import Expenses from './pages/Expenses';
 import Dashboard from './pages/Dashboard';
 import AddNewEntryForm from './components/Dashboard/AddNewEntry';
 
-const client = new ApolloClient({
-    request: operation => {
-        const token = localStorage.getItem('id_token');
 
-        operation.setContext({
-            headers: {
-                authorization: token ? `Bearer ${token}` : ''
-            }
-        })
-    },
+// Construct our main GraphQL API endpoint
+const httpLink = createHttpLink({
     uri: '/graphql',
+  });
+  
+  // Construct request middleware that will attach the JWT token to every request as an `authorization` header
+  const authLink = setContext((_, { headers }) => {
+    // get the authentication token from local storage if it exists
+    const token = localStorage.getItem('id_token');
+    // return the headers to the context so httpLink can read them
+    return {
+      headers: {
+        ...headers,
+        authorization: token ? `Bearer ${token}` : '',
+      },
+    };
+  });
+  
+  const client = new ApolloClient({
+    // Set up our client to execute the `authLink` middleware prior to making the request to our GraphQL API
+    link: authLink.concat(httpLink),
     cache: new InMemoryCache(),
-});
+  });
 
 function App() {
     return (
